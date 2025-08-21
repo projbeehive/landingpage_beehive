5c7b4f77e2cc099eb49aae5364e7bf10e07a2565
Gabriel Pelinsari <gabriel.ribeiro@sou.inteli.edu.br>
2025-08-21 17:43:22 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  output: 'export',        // <— IMPORTANTE para gerar a pasta "out/"
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 }
-
 export default nextConfig
